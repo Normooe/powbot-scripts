0fbd5e8b522dd2fbcb3a1ac135caa55351de307b
gavinnn101 <41026935+gavinnn101@users.noreply.github.com>
2022-10-27 01:57:04 -0700

    GetWorlds.py now gets worlds straight from url

--- Util/GetWorlds.py
+++ Util/GetWorlds.py
@@ -1,15 +1,21 @@
 # https://oldschool.runescape.wiki/w/Server?action=edit&section=3
 # Copy paste all the world lines from above source into file that gets opened below and run script.
 
+import requests
+
+link = "https://oldschool.runescape.wiki/w/Server?action=edit&section=3"
+
+response = requests.get(link)
+text = response.text.split("\n")
+
 worlds = []
-with open("C:\\Users\\gavin\\Desktop\\worlds.txt") as f:
-	for line in f.readlines():
-		if "switch" in line:
-			# Don't include worlds that change for whatever reason (pvp/leagues/etc)
-			pass
-		elif "mems=yes" in line:
-			world_number = line.split("|")[1]
-			worlds.append(int(world_number))
+for line in text:
+	if "switch" in line:
+		# Don't include worlds that change for whatever reason (pvp/leagues/etc)
+		pass
+	elif "mems=yes" in line:
+		world_number = line.split("|")[1]
+		worlds.append(int(world_number))
 
 
 print("Worlds:")
